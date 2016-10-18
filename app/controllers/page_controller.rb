8fc642250ca187932894f593b7958888ee79be5a
plombix <plombix@tour>
2016-10-18 16:52:43 +0200

    debug assets

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,24 +1,24 @@
 class PageController < ApplicationController
-before_action :get_student ,only: :evaluation
-before_action :get_eval ,only: :evaluation
+  before_action :get_student, only: :evaluation
+  before_action :get_eval, only: :evaluation
 
   def home
-  	@all_eval ||= Eval.all
+    @all_eval ||= Eval.all
   end
 
   def evaluation
     # binding.pry
-  	@questions = @eval.questions.sort
-    @answers = @questions.map{|quest| [ quest.statement ,user_id: current_user.id, question_id: quest.id]}
+    @questions = @eval.questions.sort_by { rand }
+    @answers = @questions.map { |quest| [quest.statement, user_id: current_user.id, question_id: quest.id] }
   end
 
+  private
 
-  private 
-  
   def get_student
-  	@student = current_user
+    @student = current_user
   end
+
   def get_eval
-  	@eval = Eval.find(params[:id])
+    @eval = Eval.find(params[:id])
   end
 end
